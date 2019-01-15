1fdf06c8dcce54aaef9dbc61d105ee18ace65d9c
Jero <tokidokitalkyou@gmail.com>
2019-01-15 17:22:50 +0800

    Planning coukey.py
    This should be created first before the GUI but will keep working on this in the coming coding sessions.

--- src/core/coukey.py
+++ src/core/coukey.py
@@ -1,6 +1,32 @@
-# Core operations of Coukey
+""" Core operations of Coukey
+
++ start_listening()
++ stop_listening()
++ tally()
+
+>>> import coukey
+>>> coukey.start_listening()    # coukey will start listening and tallying key pressed
+Listening and tallying key pressed...
+>>> coukey.tally('w')
+key: w
+frequency: 1
+total key pressed: 51
+
+"""
+
+def start_listening():
+    """ Start listening and tallying key pressed """
+
+    pass
 
 
-def last_key_typed():
+def stop_listening():
+    """ Stop listening and tallying key pressed """
 
-    return True
+    pass
+
+
+def tally():
+    """ Tally results of key pressed """
+    
+    pass
