00e49146606e0ff89e3b6ac75330f76d9f86f2e9
Jero <tokidokitalkyou@gmail.com>
2019-01-16 17:41:14 +0800

    Struggling with TDD
    Still learning how to test a GUI application. Googling some tutorials...

--- test/coukeytest.py
+++ test/coukeytest.py
@@ -3,8 +3,8 @@
 import sys
 sys.path.append('..')
 import unittest
+from collections import Counter
 import keyboard
-
 from PyQt5.QtCore import Qt
 
 # [] TODO: start testing coukey.py
@@ -25,8 +25,11 @@ class CoukeyTest(unittest.TestCase):
     def setUp(self):
 
         self.window = CoukeyWindow()
+        # main()
         self.TEST_LAST_KEY = 'enter'
+        self.TEST_LAST_KEY_FREQUENCY = 0
         self.TEST_KEY_TOTAL = 0
+        self.TEST_KEY_COUNTER = Counter()
         keyboard.hook(self.__test_key_listener)
 
     def __test_key_listener(self, event):
@@ -35,14 +38,27 @@ class CoukeyTest(unittest.TestCase):
         if event.event_type == 'down':
             self.TEST_KEY_TOTAL += 1
             self.TEST_LAST_KEY = event.name
-            print(f'[TEST] {self.TEST_KEY_TOTAL}: {self.TEST_LAST_KEY}')
+
+            if self.TEST_LAST_KEY not in self.TEST_KEY_COUNTER.keys():
+                self.TEST_KEY_COUNTER[self.TEST_LAST_KEY] = 1
+            else:
+                self.TEST_KEY_COUNTER[self.TEST_LAST_KEY] += 1
+
+            self.TEST_LAST_KEY_FREQUENCY = self.TEST_KEY_COUNTER[self.TEST_LAST_KEY]
+
+            print(f'[TEST] {self.TEST_KEY_TOTAL}: {self.TEST_LAST_KEY} ({self.TEST_LAST_KEY_FREQUENCY})')
       
     def test_main(self):
         """ Test if the app exited gracefully """
 
         self.assertEqual(main(), 0)
 
-    def test_CoukeyWindow_windowTitle_version(self):
+    def test_keyFrequencyLabel_text(self):
+
+        self.assertEqual(self.window.LAST_KEY_FREQUENCY, self.TEST_LAST_KEY_FREQUENCY)  # [] TODO: test passing, result is 0 == 0 
+
+    # [x] TODO: refactor this to ~properties
+    def test_CoukeyWindow_properties(self):
 
         self.assertEqual(self.window.windowTitle(), f'{__test_appname__} {__test_version__}')
     
