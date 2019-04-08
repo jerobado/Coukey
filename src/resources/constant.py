1c31413422a96eb0100eb490decb50b9a0375f1a
Jero <tokidokitalkyou@gmail.com>
2019-04-09 04:11:32 +0800

    Fix FileNotFoundError while app is running in freeze mode
    The error was due to not adding the qss file during packaging.

--- src/resources/constant.py
+++ src/resources/constant.py
@@ -1,13 +1,30 @@
 # Static/constant variables should be coded here
 
+import os
 import sys
 from PyQt5.QtWidgets import QApplication
 
 __appname__ = 'Coukey'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 APP = QApplication(sys.argv)
 
 
-COUKEY_QSS_FILE = open('..\qss\coukey.qss').read()
+# [] TODO: make this part of the __init__ process to identify dev or live mode
+def resource_path(relative_path):
+    """ Get absolute path of data files. """
 
+    try:
+        print('we are skiing in freeze mode # no pun intended :) #live')
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        print('we are not freeze yet #developing')
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+# development files
+# COUKEY_QSS_FILE = open('..\qss\coukey.qss').read()
+
+# # live files
+COUKEY_QSS_FILE = open(resource_path('qss\coukey.qss')).read()
